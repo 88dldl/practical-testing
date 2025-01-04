888525b03fef04e27acea2c35698843d197acca7
88dldl <chltnrgml0999@gmail.com>
2025-01-04 20:48:23 +0900

    test: 주문 생성시 총 금액 계산하는 테스트 작성

---
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -0,0 +1,39 @@
+package sample.cafekiosk.spring.domain.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.spring.domain.product.Product;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sample.cafekiosk.spring.domain.product.ProductSellingType.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+class OrderTest {
+    @DisplayName("주문 생성시 상품 리스트에서 주문의 총 금액을 계산한다.")
+    @Test
+    void calculateTotalPrice() {
+        //given
+        List<Product> products = List.of(createProduct("001", 4000),
+                createProduct("002", 1000),
+                createProduct("003", 2000)
+        );
+
+        //when
+        Order order = Order.create(products);
+
+        //then
+        assertThat(order.getTotalPrice()).isEqualTo(7000);
+    }
+
+    private Product createProduct(String productNumber, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(HANDMADE)
+                .sellingType(SELLING)
+                .name("아메리카노")
+                .price(price)
+                .build();
+    }
+}
