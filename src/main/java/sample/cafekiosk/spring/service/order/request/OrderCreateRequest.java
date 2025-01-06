54869d812cd83c71c6cf5c2d94ac0cf5eb8a5d7d
88dldl <chltnrgml0999@gmail.com>
2025-01-07 05:40:22 +0900

    feat: OrderCreateRequest에 생성자 관련 어노테이션 추가

--- src/main/java/sample/cafekiosk/spring/service/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/service/order/request/OrderCreateRequest.java
@@ -2,10 +2,12 @@ package sample.cafekiosk.spring.service.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
     List<String> productNumbers;
 
