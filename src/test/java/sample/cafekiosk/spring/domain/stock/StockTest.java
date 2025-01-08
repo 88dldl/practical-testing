3fb53a2a106f76bc58a73ed7329ded5bd993562a
88dldl <chltnrgml0999@gmail.com>
2025-01-09 06:42:12 +0900

    test: 재고보다 많은 수량으로 차감을 시도하는 경우에 대한 예외 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StockTest {
     @DisplayName("재고의 수량이 제공된 수량보다 적은지 확인한다.")
@@ -32,5 +33,18 @@ class StockTest {
 
         //then
         assertThat(stock.getQuantity()).isEqualTo(1);
+    }
+
+    @DisplayName("재고보다 많은 수의 수량으로 차감을 시도하는 경우 예외가 발생한다.")
+    @Test
+    void deductQuantity2() {
+        //given
+        Stock stock = Stock.create("001", 1);
+        int quantity = 2;
+
+        //when //then
+        assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차감할 재고 수량이 없습니다.");
     }
 }
