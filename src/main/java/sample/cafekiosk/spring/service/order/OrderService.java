8c1e3130f59293b4d4b55312d4162e26a7c1a2f1
88dldl <chltnrgml0999@gmail.com>
2025-01-09 06:59:19 +0900

    feat: 해당되는 재고를 차감하는 기능 구현

--- src/main/java/sample/cafekiosk/spring/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/service/order/OrderService.java
@@ -36,10 +36,23 @@ public class OrderService {
 
         // 재고 엔티티 조회
         List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<String, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
 
         // 상품별 개수 counting
         Map<String, Long> productCountingMap = stockProductNumbers.stream()
                 .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
+        // 재고 차감 시도
+        for (String stockProductNumber : stockProductNumbers) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            if (stock.isQuantityLessThan(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
