d199c06926f5d7b07b513592de84ce38d63a428d
88dldl <chltnrgml0999@gmail.com>
2025-01-09 07:39:23 +0900

    test: 주문 생성시 재고 처리 예외 상황에 대한 테스트 작성

--- src/test/java/sample/cafekiosk/spring/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/service/order/OrderServiceTest.java
@@ -20,6 +20,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingType.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.BAKERY;
@@ -162,6 +163,31 @@ class OrderServiceTest {
                         tuple("001", 0),
                         tuple("002", 1)
                 );
+    }
+
+    @DisplayName("재고가 없는 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock() {
+        //given
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 2000);
+        Product product3 = createProduct(HANDMADE, "003", 3000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 1);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        LocalDateTime registerDateTime = LocalDateTime.now();
+
+        //when //then
+        assertThatThrownBy(() -> orderService.createOrder(request, registerDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
     }
 
     private Product createProduct(ProductType type, String productNumber, int price) {
