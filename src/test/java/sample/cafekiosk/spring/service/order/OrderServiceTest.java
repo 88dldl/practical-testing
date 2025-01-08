a2976e61d320e2acaaa2c9204d162eec45fbc7fa
88dldl <chltnrgml0999@gmail.com>
2025-01-09 07:05:40 +0900

    test: 주문 생성시 재고 차감하는 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/service/order/OrderServiceTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.repository.order.OrderRepository;
 import sample.cafekiosk.spring.repository.orderProduct.OrderProductRepository;
 import sample.cafekiosk.spring.repository.product.ProductRepository;
+import sample.cafekiosk.spring.repository.stock.StockRepository;
 import sample.cafekiosk.spring.service.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.service.order.response.OrderResponse;
 
@@ -20,6 +22,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingType.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.BAKERY;
+import static sample.cafekiosk.spring.domain.product.ProductType.BOTTLE;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 @ActiveProfiles("test")
@@ -31,6 +35,8 @@ class OrderServiceTest {
     private OrderRepository orderRepository;
     @Autowired
     private OrderProductRepository orderProductRepository;
+    @Autowired
+    private StockRepository stockRepository;
     @Autowired
     private OrderService orderService;
 
@@ -106,6 +112,52 @@ class OrderServiceTest {
                 .containsExactlyInAnyOrder(
                         tuple("001", 1000),
                         tuple("001", 1000)
+                );
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock() {
+        //given
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 2000);
+        Product product3 = createProduct(HANDMADE, "003", 3000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        LocalDateTime registerDateTime = LocalDateTime.now();
+
+        //when
+        OrderResponse response = orderService.createOrder(request, registerDateTime);
+
+        //then
+        assertThat(response.getId()).isNotNull();
+        assertThat(response)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registerDateTime, 7000);
+        assertThat(response.getProducts())
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 2000),
+                        tuple("003", 3000)
+                );
+
+        // 재고 체크
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
                 );
     }
 
