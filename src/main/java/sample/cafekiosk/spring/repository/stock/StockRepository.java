70951aa79e69a2fa5e94a24d7a73885543da4c0f
88dldl <chltnrgml0999@gmail.com>
2025-01-09 06:04:05 +0900

    feat: 상품 리스트로 재고를 조회하는 기능 추가

--- src/main/java/sample/cafekiosk/spring/repository/stock/StockRepository.java
+++ src/main/java/sample/cafekiosk/spring/repository/stock/StockRepository.java
@@ -3,5 +3,8 @@ package sample.cafekiosk.spring.repository.stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sample.cafekiosk.spring.domain.stock.Stock;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    List<Stock> findAllByProductNumberIn(List<String> productNumbers);
 }
