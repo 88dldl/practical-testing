bd07d3f7890e55dbd11dc0dc34119e0baa807182
88dldl <chltnrgml0999@gmail.com>
2025-01-11 08:06:09 +0900

    feat: 상품 등록 요청 dto 생성

---
+++ src/main/java/sample/cafekiosk/spring/controller/product/dto/request/ProductCreateRequest.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.spring.controller.product.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import sample.cafekiosk.spring.domain.product.ProductSellingType;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@Getter
+public class ProductCreateRequest {
+    private ProductType type;
+    private ProductSellingType sellingType;
+    private String name;
+    private int price;
+
+    @Builder
+    public ProductCreateRequest(ProductType type, ProductSellingType sellingType, String name, int price) {
+        this.type = type;
+        this.sellingType = sellingType;
+        this.name = name;
+        this.price = price;
+    }
+}
