86da4098fbbba173fbd3588c91e3f136a234ab9b
88dldl <chltnrgml0999@gmail.com>
2025-01-11 08:17:23 +0900

    test: 신규 등록 시 예외 처리 테스트 코드 추가

--- src/test/java/sample/cafekiosk/spring/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/service/product/ProductServiceTest.java
@@ -54,6 +54,26 @@ class ProductServiceTest {
 
     }
 
+    @DisplayName("상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다.")
+    @Test
+    void createProductWhenProductsIsEmpty() {
+        //given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(HANDMADE)
+                .sellingType(SELLING)
+                .name("카푸치노")
+                .price(5000)
+                .build();
+
+        //when
+        ProductResponse productResponse = productService.createProduct(request);
+
+        //then
+        assertThat(productResponse).extracting("productNumber", "type", "sellingType", "name", "price")
+                .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
+
+    }
+
     private Product createProduct(String ProductNumber, ProductType type, ProductSellingType sellingType, String name, int price) {
         return Product.builder()
                 .productNumber(ProductNumber)
