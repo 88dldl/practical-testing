2f05dcb4f2bee2ee1d86629f90794023e783f6f8
88dldl <chltnrgml0999@gmail.com>
2025-01-11 08:31:45 +0900

    test: 저장 후 제품 속성 값 검증을 위한 테스트 추가

--- src/test/java/sample/cafekiosk/spring/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/service/product/ProductServiceTest.java
@@ -16,6 +16,7 @@ import sample.cafekiosk.spring.service.product.response.ProductResponse;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingType.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
@@ -45,13 +46,21 @@ class ProductServiceTest {
                 .name("카푸치노")
                 .price(5000)
                 .build();
+
         //when
         ProductResponse productResponse = productService.createProduct(request);
+        List<Product> products = productRepository.findAll();
 
         //then
         assertThat(productResponse).extracting("productNumber", "type", "sellingType", "name", "price")
                 .contains("002", HANDMADE, SELLING, "카푸치노", 5000);
 
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "type", "sellingType", "name", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", HANDMADE, SELLING, "아메리카노", 4000),
+                        tuple("002", HANDMADE, SELLING, "카푸치노", 5000)
+                );
     }
 
     @DisplayName("상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다.")
@@ -67,11 +76,17 @@ class ProductServiceTest {
 
         //when
         ProductResponse productResponse = productService.createProduct(request);
+        List<Product> products = productRepository.findAll();
 
         //then
         assertThat(productResponse).extracting("productNumber", "type", "sellingType", "name", "price")
                 .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
 
+        assertThat(products)
+                .extracting("productNumber", "type", "sellingType", "name", "price")
+                .contains(
+                        tuple("001", HANDMADE, SELLING, "카푸치노", 5000)
+                );
     }
 
     private Product createProduct(String ProductNumber, ProductType type, ProductSellingType sellingType, String name, int price) {
