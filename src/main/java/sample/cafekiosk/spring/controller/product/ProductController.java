5cd47e53ebe4df79b0b42f689fd5df379c7bddca
88dldl <chltnrgml0999@gmail.com>
2025-01-12 05:18:38 +0900

    feat: 요청 파라미터 검증을 위한 어노테이션 추가

--- src/main/java/sample/cafekiosk/spring/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/controller/product/ProductController.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.controller.product;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public ProductResponse createProduct(@RequestBody ProductCreateRequest request) {
+    public ProductResponse createProduct(@Valid @RequestBody ProductCreateRequest request) {
         return productService.createProduct(request);
     }
 
