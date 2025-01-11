6f685c28c997fa2578e2f55720eef8f600d09eeb
88dldl <chltnrgml0999@gmail.com>
2025-01-12 06:15:02 +0900

    feat: 응답 구조 통일을 위해 ApiResponse 래핑 적용

--- src/main/java/sample/cafekiosk/spring/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/controller/product/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.ApiResponse;
 import sample.cafekiosk.spring.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.service.product.ProductService;
 import sample.cafekiosk.spring.service.product.response.ProductResponse;
@@ -18,8 +19,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public ProductResponse createProduct(@Valid @RequestBody ProductCreateRequest request) {
-        return productService.createProduct(request);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.oK(productService.createProduct(request));
     }
 
     @GetMapping("/api/v1/products/selling")
