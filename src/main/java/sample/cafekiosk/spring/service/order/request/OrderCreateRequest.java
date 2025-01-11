42fe699dd90199f6e0a0ffe595df356bf167d3d3
88dldl <chltnrgml0999@gmail.com>
2025-01-12 06:51:23 +0900

    feat: 유효성 검증을 위한 어노테이션 추가

--- src/main/java/sample/cafekiosk/spring/service/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/service/order/request/OrderCreateRequest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.service.order.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class OrderCreateRequest {
+    @NotEmpty(message = "상품 번호 리스트는 필수입니다.")
     List<String> productNumbers;
 
     @Builder
